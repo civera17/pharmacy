cc58dc177db9916b0172d5ce2b95aba925d1840a
Saketh <alurusaisaketh@gmail.com>
2019-03-19 11:26:34 +0530

    Template for login page

--- login.php
+++ login.php
@@ -3,17 +3,53 @@
 <head>
 	<title> LOGIN </title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 <center>
 	
 <h3>Login</h3>
-<form action="login-backend.php" method="POST">
+<!-- <form action="login-backend.php" method="POST">
 	Username: <input name="username" placeholder = "Username">
 	<br>
 	Password: <input type="password" name="password" placeholder = "Password">
 	<br>
-	<button type = "submit">Submit</button>
+	<button type = "submit">Submit</button> -->
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form p-l-55 p-r-55 p-t-178" action="login-backend.php" method="POST">
+					<span class="login100-form-title">
+						Sign In
+					</span>
+
+					<div class="wrap-input100 validate-input m-b-16" data-validate="Please enter username">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Please enter password">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+					</div>
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Sign in
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 </center>
 </body>
 </html>
