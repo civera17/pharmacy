6963935ef4a2f1869bc78d1450dc0d14b146b331
Saketh <alurusaisaketh@gmail.com>
2019-04-01 23:19:01 +0530

    css for forms

--- admin_add_inventory.php
+++ admin_add_inventory.php
@@ -26,22 +26,62 @@
 </head>
 <body>
 <center>
-<h3>Add inventory</h3>	
-<form action= "admin_view_inventory_backend.php" method="GET">
 	<?php
 
 	require_once 'dbconnect.php';
-
-	echo '<input type="hidden" name="identifier" value="2">';
-	echo 'Inventory ID: <input type="text" name="inventory_id"><br>';
-	echo 'Medicine ID: <input type="text" name="medicine_id"><br>';
-	echo 'Quantity: <input type="text" name="quantity"><br>';
-	echo 'Manufacture Date: <input type="text" name="manufacture_date"><br>';
-	echo 'Expiry Date: <input type="text" name="expiry_date"><br>';
-	echo 'Shelf No: <input type="text" name="shelf_no"><br>';
-	echo '<button type="submit">Add inventory</button>';
 	?>
+	<div class="container-contact100">
+		<div class="wrap-login100"  style="width: 100%">
+			<span class="contact100-form-title" style="text-align: center;">
+				Add Inventory
+			</span>
+	<form action= "admin_view_inventory_backend.php" method="GET">
+		<input type="hidden" name="identifier" value="2">
+	<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Medicine Id is required">
+		<!-- <span class="label-input100">Medicine ID</span> -->
+		<input class="input100" type="text" name="medicine_id" placeholder="Enter Medicine id" required="true">
+		<span class="focus-input100"></span>
+	</div>
+
+	<!-- echo 'Medicine ID: <input type="text" name="medicine_id"><br>'; -->
+	<!-- echo 'Quantity: <input type="text" name="quantity"><br>'; -->
+	<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Medicine quantity is required">
+		<!-- <span class="label-input100">Medicine Quantity</span> -->
+		<input class="input100" type="text" name="quantity" placeholder="Enter Medicine quantity" required="
+	true">
+		<span class="focus-input100"></span>
+	</div>
+	<!-- echo 'Manufacture Date: <input type="text" name="manufacture_date"><br>'; -->
+	<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Medicine Manufacture Date is required">
+		<!-- <span class="label-input100">Medicine Manufacture Date</span> -->
+		<input class="input100" type="text" name="manufacture_date" placeholder="Enter Medicine Manufacture Date" required="
+	true">
+		<span class="focus-input100"></span>
+	</div>
+	<!-- echo 'Expiry Date: <input type="text" name="expiry_date"><br>'; -->
+	<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Medicine Expiry Date is required">
+		<!-- <span class="label-input100">Medicine Expiry Date</span> -->
+		<input class="input100" type="text" name="expiry_date" placeholder="Enter Medicine Expiry Date" required="
+	true">
+		<span class="focus-input100"></span>
+	</div>
+	<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Shelf No. is required">
+		<input class="input100" type="text" name="shelf_no" placeholder="Enter Medicine id" required="
+	true">
+		<span class="focus-input100"></span>
+	</div>
+	<!-- <button type="submit">Add inventory</button>; -->
+	<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn" style="margin-left: 45%">
+						<span>
+							Submit
+							<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+						</span>
+					</button>
+					</div>
 </form>
+</div>
+</div>
 </center>
 </body>
 </html>
