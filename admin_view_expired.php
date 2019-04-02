60c9261f4ca2235d25ea400f874128c0f8b176d6
Chandana-pvsl <chandupvsl29@gmail.com>
2019-04-02 11:00:02 +0530

    added back to home button in admin pages

--- admin_view_expired.php
+++ admin_view_expired.php
@@ -73,7 +73,12 @@ else{
 <br>
 <div class="container-login100-form-btn">
 <!-- <button style="width: 20%; font-size: 20px" class="login100-form-btn"  type = "reset" value="Reset">Reset</button> -->
-<button style="width: 20%; font-size: 20px" class="login100-form-btn" type="button" value="del_medicine">Remove medicine</button>
+<button style="width: 20%; font-size: 15px" class="login100-form-btn" type="button" value="del_medicine">Remove medicine</button>
+</div>
+<div class="container-login100-form-btn">
+<button style="width: 20%; font-size: 15px ;
+     margin-top: 1%;  margin-right: 1%;
+     position:relative;" class="login100-form-btn" onclick = "location.href='admin_home.php'" type="button" name="Back">Back to home</button>
 </div>
 </center>
 </body>
