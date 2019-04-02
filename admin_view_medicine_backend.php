6a21d1094db341267e1756afc02806c6a5c442a2
Anusha Potnuru <anusha.sparkx@gmail.com>
2019-04-02 11:12:47 +0530

    bug fix in header

--- admin_view_medicine_backend.php
+++ admin_view_medicine_backend.php
@@ -4,24 +4,16 @@ require_once 'dbconnect.php';
 
 $identifier = $_GET["identifier"];
 
-function getAddress() {
-    $protocol = $_SERVER['HTTPS'] == 'on' ? 'https' : 'http';
-    return $protocol.'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-}
 
-$url= getAddress();
 if($identifier==0)
 {
 	$medicine_id = $_GET["medicine_id"];
 	$query = "DELETE FROM medicine WHERE medicine_id=$medicine_id";
 	$result = mysqli_query($con, $query);
 
-	$scheme = parse_url($url, PHP_URL_SCHEME);
-	$user = parse_url($url, PHP_URL_USER);
-	$pass = parse_url($url, PHP_URL_PASS);
-	$host = parse_url($url, PHP_URL_HOST);
-	$port = parse_url($url, PHP_URL_PORT);
-	header("Location: ".$scheme."://".$user.":".$pass."@".$host.":".$port."/MediKart/admin_view_medicine.php");
+	echo "<script type='text/javascript'>";
+	echo "window.location.href = 'admin_view_medicine.php';";
+	echo "</script>";
 }
 else if($identifier==1)
 {
@@ -83,13 +75,10 @@ else if($identifier==1)
 
 	printf(mysqli_error($con));
 
-	$scheme = parse_url($url, PHP_URL_SCHEME);
-	$user = parse_url($url, PHP_URL_USER);
-	$pass = parse_url($url, PHP_URL_PASS);
-	$host = parse_url($url, PHP_URL_HOST);
-	$port = parse_url($url, PHP_URL_PORT);
-	header("Location: ".$scheme."://".$user.":".$pass."@".$host.":".$port."/MediKart/admin_view_medicine.php");
-
+	// header("location.href: 'admin_view_medicine.php'");
+	echo "<script type='text/javascript'>";
+	echo "window.location.href = 'admin_view_medicine.php';";
+	echo "</script>";
 }
 else if($identifier==2)
 {
